494e6bcb45d205609dd4cb5e0bdeed2f7ba55545
jmyv08 <velarde.jm08@gmail.com>
2024-08-11 16:11:34 +0300

    Add component directories and initial Home page

---
+++ components/organisms/Navbar/index.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between">
+      <div>
+        <p>(Logo goes here)</p>
+      </div>
+      <div>
+        <p>(Log In and Sign Up buttons goes here)</p>
+      </div>
+    </nav>
+  );
+};
+
+export { Navbar };
