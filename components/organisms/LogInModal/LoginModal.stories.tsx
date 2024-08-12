f7b5fb7f1611e3bc98d4c19df73cf1176ec4683c
jmyv08 <velarde.jm08@gmail.com>
2024-08-12 09:43:07 +0300

    Add storybook

---
+++ components/organisms/LogInModal/LoginModal.stories.tsx
@@ -0,0 +1,23 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { LoginModal } from '@/components/organisms/LogInModal';
+
+const meta = {
+  title: 'Organisms/LoginModal',
+  component: LoginModal,
+  parameters: {
+    // layout: 'centered',
+  },
+} satisfies Meta<typeof LoginModal>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const closeModal = () => {
+  alert('test');
+};
+
+export const Default: Story = {
+  args: {
+    closeModal: closeModal,
+  },
+};
